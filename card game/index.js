a148bd5337ba2332e1cc4600e78c74f3f1815b44
codeNewbie08 <139654958+codenewbie08@users.noreply.github.com>
2023-07-25 03:02:23 -0500

    first

---
+++ card game/index.js
@@ -0,0 +1,72 @@
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
+let cards = [];
+let sum = 0;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+
+
+let player = {
+    name: "Nathaniel" , 
+    chips: 500 
+}
+playerEl.textContent = player.name + ": $"+ player.chips;
+
+
+function getRandomCard() {
+    let randomNumer = Math.floor ( Math.random()*13 ) + 1
+    if (randomNumer > 10 ){
+        return 10
+    } else if (randomNumer === 1) {
+        return 11
+    } else {
+        return randomNumer;
+    }
+}
+
+
+function startGame(){
+    isAlive = true;
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
+}
+
+
+function renderGame(){
+
+        cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++){
+        cardsEl.textContent += cards[i] + " "
+    }
+    
+    sumEl.textContent = "Sum: " + sum;
+    if (sum <= 20){
+        message = "Do you want to draw a new card?"
+    }
+    else if(sum === 21){
+        message = "You've got a blackjack!!"
+        hasBlackJack = true;
+    }
+    else {
+        message = "You're out of the game."
+        isAlive = false;
+    }
+    messageEl.textContent = message ; 
+
+}
+function newCard() {
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard ()
+        sum += card;
+        cards.push(card)
+        renderGame() 
+    }
+    
+}   
+
---
+++ train/trainStation/practice.js
@@ -0,0 +1,29 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment(){
+    count += 1;
+    countEl.textContent = count ;  
+    
+}
+function save(){
+    let countStr = count + " - ";
+    saveEl.textContent += countStr;
+    countEl.textContent = 0;
+    count = 0;
+    console.log(count)
+    
+
+}
+
+
+
+
+/*
+let name = "nats"
+let greeting = "Hi, my name is "
+
+let myGreeting = greeting + name
+console.log(myGreeting)
+*/
